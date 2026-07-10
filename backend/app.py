2007045852a28b2350b115600b1f211e82dd168f
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-07-11 01:57:38 +0530

    Implement flask backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,44 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import google.generativeai as genai
+from google import genai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+print(os.getenv("GEMINI_API_KEY"))
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend is running!"
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    try:
+        data = request.get_json()
+
+        user_message = data.get("message", "")
+
+        response = client.models.generate_content(
+            model="gemini-3.5-flash",
+            contents=user_message
+        )
+
+        return jsonify({
+            "reply": response.text
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
